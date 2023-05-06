76a3540aa168d8284a7d33547649149b77f187f4
vaibhaws1 <33689782+vaibhaws1@users.noreply.github.com>
2023-05-06 13:05:52 -0400

    Update testELEN_E6883_NFT.js
     //*** VK added code #3 ***

--- test/testELEN_E6883_NFT.js
+++ test/testELEN_E6883_NFT.js
@@ -32,4 +32,41 @@ contract("ELEN_E6883_NFT", accounts => {
         assert.equal(name, result.name, "The name was not stored.");
         assert.equal(desc, result.description, "The description was not stored.");
     });
+    
+    //*** VK added code #3 ***
+    it("TC2: transfer ownership.", async () => {
+
+        const currOwner = await getOwner(1216);
+        await setOwner(1218, msg.sender);
+        const newOwner = await getOwner(1218);
+
+        assert.notEqual(currOwner, newOwner, "The owners are not distinct.");
+    });
+    
+    it("TC3: List NFT.", async () => {
+        await createSale (1216, 110)
+        const price = await getPrice(1216);
+        assert.equal(price, 1216, "The price does not match.");
+    });
+    
+    it("TC4: delist NFT.", async () => {
+        await delistNFT (1216)
+        const aOwner = await getOwner(1216);
+        assert.equal(aOwner, NaN, "Delist NFT was not done.");
+    });
+    
+    it("TC5: Sell NFT.", async () => {
+        const currOwner = await getOwner(1216);
+        await sellNFT (1216);
+        const newOwner = await getOwner(1216);
+        assert.notEqual(currOwner, newOwner, "Sell unsucessful.");
+    });
+
+    it("TC6: Unsuccessful Sell NFT.", async () => {
+        const currOwner = await getOwner(1216);
+        await cancelSale (1216);
+        const newOwner = await getOwner(1216);
+        assert.equal(currOwner, newOwner, "Sell Cancel order failed.");
+    });
+    //*** VK end code #3 ***   
 });
